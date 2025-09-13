bfb8cc5ea21925b412de3328159167d2168acab8
ItzJunaid2020 <jh7538694@gmail.com>
2025-09-13 13:25:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,34 @@ function App() {
   return (
     <>
      
-      <h1 className="text-3xl font-bold text-red-500">Lecture 1</h1>
-      
+    <div className="bg-white-500 w-100 h-80 flex ml-120 mt-30 ">
+    <div className="bg-blue-500 w-1/3 "></div>
+    <div className="bg-white-500 w-1/3"></div>
+    <div className="bg-red-500 w-1/3"></div>
+      </div>
+    <div className="bg-white-600 w-150 h-80 flex-row  ml-100 mt-30 ">
+    <div className="bg-blue-600 h-1/6 "></div>
+    <div className="bg-white-600 h-1/6"></div>
+    <div className="bg-red-500 h-2/6 "></div>
+    <div className="bg-white-600 h-1/6 "></div>
+    <div className="bg-blue-600 h-1/6"></div>
+      </div>
+      <div className="bg-white-600 w-100 h-70 flex flex-col  items-center justify-center  border-1  ">
+        <div className="bg-red-600 w-40 h-40 rounded-full ">s</div>
+      </div>
+
+      <div className="bg-white-600 w-150 h-80 flex-row  ml-100 mt-30 ">
+    <div className="bg-blue-600 h-1/6 "></div>
+    <div className="bg-white-600 h-1/6 flex flex-col  items-center justify-center">
+    <div className="bg-green-600 w-13 h-20 rounded-full "></div></div>
+    <div className="bg-red-500 h-2/6 "></div>
+    <div className="bg-white-600 h-1/6 "></div>
+    <div className="bg-blue-600 h-1/6"></div>
+      </div>
+
+       <div className="bg-green-600 w-100 h-70 flex flex-col  items-center justify-center  border-1  ">
+        <div className="bg-red-600 w-40 h-40 rounded-full ">s</div>
+      </div>
     </>
   )
 }
